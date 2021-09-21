f1796c35b305143eb57f40e3c528d959d8ca177d
Elijah Cobb <elijah@elijahcobb.com>
2021-09-21 17:17:31 -0400

    sync

--- ts/index.ts
+++ ts/index.ts
@@ -14,6 +14,14 @@ function notImplemented(): Error {
 // https://parseplatform.org/Parse-SDK-JS/api/master/Parse.Object.html
 export class ParseObject {
 
+	private object: Parse.Object<{name?: string, age: number}>;
+
+	public constructor(className: string) {
+		this.object = new Parse.Object(className);
+		this.object.get("name")
+	}
+
+
 	public addAllUnique(key: string, ...items: any[]): void {}
 	public addUnique(key: string, item: any): void {}
 	public clear(): void {}
@@ -23,8 +31,6 @@ export class ParseObject {
 	public dirty(key?: string): boolean { throw notImplemented(); }
 	public dirtyKeys(): string[] { throw notImplemented(); }
 	public equals(other: ParseObject): boolean { throw notImplemented(); }
-	public escape(key: string): string { throw notImplemented(); }
-	public existed(): boolean { throw notImplemented(); }
 	public async exists(): Promise<boolean> { throw notImplemented(); }
 	public async fetch(): Promise<void> {}
 	public async fetchFromLocalDatastore(): Promise<void> {}
@@ -33,6 +39,12 @@ export class ParseObject {
 	public has(key: string): boolean { throw notImplemented(); }
 	public increment(key: string, amount: number = 1): void {}
 	public async isPinned(): Promise<boolean> { throw notImplemented(); }
-
+	public async pin(): Promise<void> {}
+	public async pinWithName(name: string): Promise<void> {}
+	public setACL(acl: Parse.ACL): void {}
+	public toJSON(): void {}
+	public async unPin(): Promise<void> {}
+	public async unPinWithName(name: string): Promise<void> {}
+	public parseObject(): Parse.Object { return this.object; }
 
 }
